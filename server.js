a12220c5f8538e7975242ff1486662cc42f76df4
Thomas Holley <thomas.holley@att.net>
2016-02-06 15:49:22 -0600

    Update language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 app.get('/about', function (req, res) {
-	res.send('About Us');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
